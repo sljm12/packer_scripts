aafff86fe1d4b51149121117b8d109215260e48b
stephenleejm <stephenleejm@gmail.com>
2021-04-29 09:08:57 +0800

    custom.json - Packer script for making additional changes to the VM to harden the last 10%
    rules.sh - additional hardening script

---
+++ Centos7-VirtualBox/apply_custom_rules/rules.sh
@@ -0,0 +1,20 @@
+#1.1.17 Ensure noexec option set on /dev/shm partition
+echo "tmpfs /dev/shm tmpfs defaults,nodev,nosuid,noexec 0 0" >> /etc/fstab
+
+#3.2.4 Ensure suspicious packets are logged
+sysctl -w net.ipv4.conf.all.secure_redirects=0
+
+sysctl -w net.ipv4.conf.default.secure_redirects=0
+
+sysctl -w net.ipv4.route.flush=1 
+
+#3.4.3 Ensure /etc/hosts.deny is configured
+echo "ALL: ALL" >> /etc/hosts.deny
+echo "sshd: ALL" >> /etc/hosts.allow 
+
+#5.2.14 Ensure SSH access is limited
+echo -e "\nAllowUsers admin\n" >> /etc/ssh/sshd_config
+
+#5.4.5 Ensure default user shell timeout is 900 seconds or less
+echo -e "\nTMOUT=600\n" >> /etc/bashrc
+echo -e "\nTMOUT=600\n" >> /etc/profile
