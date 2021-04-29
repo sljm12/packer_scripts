374c20973a29a31a4e5f7b8c9a2ce7bc0b81fd8e
stephenleejm <stephenleejm@gmail.com>
2021-04-29 11:04:30 +0800

    addednew rules for setting the ICMP requests

--- Centos7-VirtualBox/apply_custom_rules/rules.sh
+++ Centos7-VirtualBox/apply_custom_rules/rules.sh
@@ -1,6 +1,10 @@
 #1.1.17 Ensure noexec option set on /dev/shm partition
 echo "tmpfs /dev/shm tmpfs defaults,nodev,nosuid,noexec 0 0" >> /etc/fstab
 
+#3.2.3 Ensure secure ICMP redirects are not accepted
+sed -i 's/net.ipv4.conf.all.secure_redirects = 1/net.ipv4.conf.all.secure_redirects = 0/' /etc/sysctl.conf
+sed -i 's/net.ipv4.conf.default.secure_redirects = 1/net.ipv4.conf.default.secure_redirects = 0/' /etc/sysctl.conf
+
 #3.2.4 Ensure suspicious packets are logged
 sysctl -w net.ipv4.conf.all.secure_redirects=0
 
@@ -11,6 +15,8 @@ sysctl -w net.ipv4.route.flush=1
 #3.4.3 Ensure /etc/hosts.deny is configured
 echo "ALL: ALL" >> /etc/hosts.deny
 
+#4.2.1.3 Ensure rsyslog default file permissions configured
+echo '$FileCreateMode 0640' >> /etc/rsyslog.conf
 
 #5.2.14 Ensure SSH access is limited
 echo -e "\nAllowUsers admin\n" >> /etc/ssh/sshd_config
@@ -21,3 +27,5 @@ echo -e "\nTMOUT=600\n" >> /etc/profile
 
 #Rule to allow SSHD
 echo "sshd: ALL" >> /etc/hosts.allow 
+
+
